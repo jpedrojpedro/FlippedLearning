f1331b4e9a0a0b337c2d59791788a2d0da0d9e48
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2014-01-14 11:31:39 -0200

    Toda a dinamica de criacao de esquema por usuario esta funcionando. Toda a logica do funcionamento esta na view questao. O resultado esta sendo exibido na tela question_page, mas tenho que bolar uma maneira de trazer todos os resultados anteriores.

--- database_app/views/questao.py
+++ database_app/views/questao.py
@@ -7,6 +7,8 @@ from django.db import connection, transaction
 from database_app.models.questao import Questao
 from database_app.models.instancia import Instancia
 from database_app.models.esquema import Esquema
+from database_app.models.usuario import Usuario
+from datetime import datetime
 
 @login_required
 def verificar(request, exercicio_id, questao_id):
@@ -15,50 +17,61 @@ def verificar(request, exercicio_id, questao_id):
         # Obtendo a questão respondida
         q = Questao.objects.get(id=questao_id)
         schema_name = "%s_%s" % (q.nome_esquema, request.user)
+        login = "%s" % request.user
         # Verificando se a instancia existe
-        i = Instancia.objects.filter(nome_esquema=q.nome_esquema, login_usuario=request.user)
+        i = Instancia.objects.filter(nome_esquema=q.nome_esquema, login_usuario=login)
         # Caso em que a instancia existe
         if i:
-            cursor = connection.cursor()
-            command = "set search_path to %s" % schema_name
-            cursor.execute(command)
-            transaction.commit_unless_managed()
-            # Trecho igual -- Otimizar
-            cursor.execute(q.resposta_gabarito)
-            answer = cursor.fetchall()
-            cursor.execute(r)
-            answer_user = cursor.fetchall()
-            if answer == answer_user:
-                result = True
-            else:
-                result = False
-            cursor.execute("set search_path to public")
-            transaction.commit_unless_managed()
+            trocar_esquema(schema_name)
+            result = comparar_respostas(r, q.resposta_gabarito)
         # Caso em que a instancia não existe -- Salvar na tabela!
         else:
             e = Esquema.objects.get(nome=q.nome_esquema)
-            cursor = connection.cursor()
             command = "create schema %s" % schema_name
-            cursor.execute(command)
-            transaction.commit_unless_managed()
-            command = "set search_path to %s" % schema_name
-            cursor.execute(command)
-            transaction.commit_unless_managed()
-            cursor.execute(e.criacao)
-            transaction.commit_unless_managed()
-            # Trecho igual -- Otimizar
-            cursor.execute(q.resposta_gabarito)
-            answer = cursor.fetchall()
-            cursor.execute(r)
-            answer_user = cursor.fetchall()
-            if answer == answer_user:
-                result = True
-            else:
-                result = False
-            cursor.execute("set search_path to public")
-            transaction.commit_unless_managed()
+            executar_comando(command)
+            trocar_esquema(schema_name)
+            executar_comando(e.criacao)
+            result = comparar_respostas(r, q.resposta_gabarito)
+            u = Usuario.objects.get(login=request.user)
+            i = Instancia()
+            i.nome_esquema = e
+            i.nome = schema_name
+            i.login_usuario = u
+            i.dt_criacao = datetime.now()
+            i.save()
         template = loader.get_template("question_page.html")
         context = RequestContext(request, {
             'answer': result,
         })
         return HttpResponse(template.render(context))
+
+
+def comparar_respostas(resposta_aluno, resposta_gabarito):
+    cursor = connection.cursor()
+    cursor.execute(resposta_gabarito)
+    correct_answer = cursor.fetchall()
+    cursor.execute(resposta_aluno)
+    user_answer = cursor.fetchall()
+    if user_answer == correct_answer:
+        result = True
+    else:
+        result = False
+    cursor.execute("set search_path to public")
+    transaction.commit_unless_managed()
+    cursor.close()
+    return result
+
+
+def trocar_esquema(nome_esquema):
+    cursor = connection.cursor()
+    command = "set search_path to %s" % nome_esquema
+    cursor.execute(command)
+    transaction.commit_unless_managed()
+    cursor.close()
+
+
+def executar_comando(sql):
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    transaction.commit_unless_managed()
+    cursor.close()
