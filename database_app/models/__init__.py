0f087227f44d11e09f8cd23b5a1e1cea537541dd
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2014-01-14 20:59:56 -0200

    Ajustando a ordem de insercao dos models no init.

--- database_app/models/__init__.py
+++ database_app/models/__init__.py
@@ -1,17 +1,18 @@
 # coding=utf-8
 
+from usuario import Usuario
+from lista_exercicio import ListaExercicio
+from esquema import Esquema
+from questao import Questao
 from assunto import Assunto
+from duvida import Duvida
 from assunto_duvida import AssuntoDuvida
+
 from assunto_lista_exercicio import AssuntoListaExercicio
 from comentario import Comentario
-from duvida import Duvida
-from esquema import Esquema
 from instancia import Instancia
-from lista_exercicio import ListaExercicio
 from modificacao import Modificacao
-from questao import Questao
 from resposta import Resposta
-from usuario import Usuario
 from usuario_modera_comentario import UsuarioModeraComentario
 from usuario_modera_duvida import UsuarioModeraDuvida
 from visualizacao import Visualizacao
